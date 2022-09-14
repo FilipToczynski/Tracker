5325f6b572a85728c111064e366f12ec010d0d7c
FilipToczynski <ftoczynski1998@gmail.com>
2022-09-14 16:36:30 +0200

    changed start

---
+++ src/window/Window.js
@@ -0,0 +1,30 @@
+
+
+function Window() {
+    return (
+        <div></div>
+    )
+}
+
+export default Window;
+
+
+
+
+
+// 
+// 
+// current project    ////////////////////////////////////////////////////////////////////////////////////////
+//                    building window tracker no 01-22s start end
+// projects +
+// - tracker
+// -
+// -
+//                                                          this week: {time spent on the clock}
+//                                                          money summary: {money made}
+//                  list of tasks above here 
+
+// to extract from this file
+// 
+// 
+// 
