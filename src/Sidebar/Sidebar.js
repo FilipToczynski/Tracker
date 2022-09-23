413fb68d772267dbbb1a79e8c50e724c159708e8
FilipToczynski <ftoczynski1998@gmail.com>
2022-09-23 18:44:42 +0200

    local storage data

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -1,7 +1,8 @@
 import { IconContext } from "react-icons";
 import styles from "./Sidebar.module.css";
-import { GrFormAdd } from "react-icons/gr";
 import { useEffect, useRef, useState } from "react";
+import { GrFormAdd } from "react-icons/gr";
+import { BsTrash } from "react-icons/bs";
 
 function Sidebar() {
   const projectArray = ["dummy"];
@@ -9,54 +10,80 @@ function Sidebar() {
 
   const inputRef = useRef(null);
   const [input, setInput] = useState("");
+  const [modal, setModal] = useState(false);
 
-  function addTask() {
+  function addProject() {
     const projectExists = projectsList.includes(inputRef.current.value);
+    let lastIndex = projectsList.length - 1;
+    let name = inputRef.current.value;
+    let dateToday = new Date();
+    let date = dateToday.getFullYear() +"-" +(dateToday.getMonth() + 1) +"-" +dateToday.getDate();
+    let timeToday = new Date();
+    let time =timeToday.getHours() +":" +timeToday.getMinutes() +":" +timeToday.getSeconds();
 
     if (projectExists) {
       window.alert("project already exists");
     } else {
-
       setProjectsList([...projectsList, inputRef.current.value]);
       localStorage.setItem(
         "allProjects",
         JSON.stringify([...projectsList, inputRef.current.value])
+      );
+
+      localStorage.setItem(
+        `${lastIndex + 1}`,
+        JSON.stringify({
+          name: name,
+          date: date,
+          time: time,
+        })
       );
     }
   }
 
   //  to reset the list
   // localStorage.setItem('allProjects', JSON.stringify(projectsList));
+  // localStorage.removeItem('2', JSON.stringify({}));
+  // localStorage.removeItem('3', JSON.stringify({}));
 
-  const pullProjectsList = () => {
-    const updatedList = JSON.parse(localStorage.getItem("allProjects"));
+//   const pullProjectsList = () => {
+//     const updatedList = JSON.parse(localStorage.getItem("allProjects"));
 
-    setProjectsList([...updatedList]);
-  };
+//     setProjectsList([...updatedList]);
+//   };
 
-  useEffect(() => {
-    pullProjectsList();
-  }, [inputRef]);
+//   useEffect(() => {
+//     pullProjectsList();
+//   }, [inputRef]);
 
-  // add current time to lrerrl storage
-  // current date
-  // name
-  // time spent on a task in total
-  // all in one object
 
-  // 1.push it to local storage and onClick on list item display the data below
-  // read {
-  //   date : curDate,
-  //   time: curTime,
-  //   name: read,
-  //   timeTotal: timeTotal,
-  //   prevTasks: look {
-  //   same as above
-  // }
-  // }
+// let indx;
+// const getId = (index) => {
+//   indx = index;
+//   console.log(indx);
+//   return indx;
+// };
 
-  // to do
-  // dont allow to make projects wit the same name
+//   const closeModal = () => {
+//     setModal(false);
+//   };
+
+//   const openModal = () => {
+//     // console.log(indx);
+//     setModal(true);
+//   };
+
+//     const deleteProject = () => {
+//       console.log(indx);
+//       localStorage.removeItem(indx, JSON.stringify({}));
+//       const updatedList = JSON.parse(localStorage.getItem("allProjects"));
+//       updatedList.splice(indx, 1);
+//       localStorage.setItem('allProjects', JSON.stringify([...updatedList]));
+//       setProjectsList([...updatedList]);
+//       closeModal();
+//     };
+
+ 
 
   return (
     <div className={styles.sidebar}>
@@ -73,7 +100,7 @@ function Sidebar() {
           ></input>
           <button
             className={styles.btn}
-            onClick={addTask}
+            onClick={addProject}
             disabled={input.length === 0 ? true : false}
           >
             Add
@@ -87,10 +114,33 @@ function Sidebar() {
       </div>
       <div>
         <h2 className={styles.heading}>Projects list</h2>
+        {modal && (
+          <div className={styles.modal}>
+            delete project?
+            <div>
+              {/* <button onClick={deleteProject}>yes</button>
+              <button onClick={closeModal}>no</button> */}
+            </div>
+          </div>
+        )}
         <ul className={styles.list}>
           {projectsList.map((project, index) => (
-            <li key={index} className={styles.item}>
+            <li
+              key={index}
+              className={styles.item}
+              // onClick={() => getId(index)}
+            >
               {project}
+              {/* onClick={openModal} */}
+              <span className={styles.delete}> 
+                <IconContext.Provider
+                  value={{ color: "#f9f9f9", className: styles.d }}
+                >
+                  <div>
+                    <BsTrash />
+                  </div>
+                </IconContext.Provider>
+              </span>
             </li>
           ))}
         </ul>
--- src/window/Window.js
+++ src/window/Window.js
@@ -43,19 +43,4 @@ function Window() {
 
 export default Window;
 
-//
-//
-// current project    ////////////////////////////////////////////////////////////////////////////////////////
-//                    building window tracker no 01-22s start end
-// projects +
-// - tracker
-// -
-// -
-//                                                          this week: {time spent on the clock}
-//                                                          money summary: {money made}
-//                  list of tasks above here
 
-// to extract from this file
-//
-//
-//
